00f8a3c8f0e86be3852928b9dd93a58279f05c60
Puja Bhattacharjee <stevieisme3290@gmail.com>
2020-04-17 23:30:49 -0400

    Function for logging in implemented.
    -Edited UserEntity to have extra property (roles)
    -Created UserRepository to get users given a username
    -MyUserDetails implements UserDetails to store info about user
    -MyUserDetailsService loads UserDetails given a username
    -SecurityConfiguration configures the application to require an authorised user when accessing '/home'
    -Edited a property about hibernate dialect to application.properties

--- levelBuilder/src/main/java/levelBuilder/com/LevelBuilderApplication.java
+++ levelBuilder/src/main/java/levelBuilder/com/LevelBuilderApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses =  UserRepository.class)
 public class LevelBuilderApplication {
 
 	public static void main(String[] args) {
