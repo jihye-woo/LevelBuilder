91906d17c4e3547dbbc2b1956db8fefe9ed343f3
Puja Bhattacharjee <stevieisme3290@gmail.com>
2020-04-23 21:23:55 -0400

    Update Layer and LayerProperties repository

--- levelBuilder/src/main/java/levelBuilder/com/repositories/LayerRepository.java
+++ levelBuilder/src/main/java/levelBuilder/com/repositories/LayerRepository.java
@@ -4,7 +4,9 @@ import levelBuilder.com.entities.LayerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LayerRepository extends JpaRepository<LayerEntity, Integer> {
-    LayerEntity findByMapName(String mapName); //find all the layers in a given map
+    List<LayerEntity> findByMapName(String mapName); //find all the layers in a given map
 }
